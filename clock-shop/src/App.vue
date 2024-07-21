6f8fa00c4d7cca2e85ecae1b1c44b23907b7b93d
Miko <MikoBN@gmail.com>
2024-07-21 10:45:07 +0500

    created front app on vue and started working with header

---
+++ clock-shop/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <header>
+      <a>Times</a>
+      <a>Watches</a>
+      <a>About us</a>
+      <a>Store</a>
+      <i class="bi bi-cart3 cart-icon"></i>
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+header a {
+  text-transform: uppercase;
+  color: #fff;
+  font-family: 'Manrope', sans-serif;
+  font-size: 18px;
+  font-weight: 300;
+}
+header .cart-icon {
+  display: inline-block;
+  transform: scaleX(-1); 
+}
+</style>
