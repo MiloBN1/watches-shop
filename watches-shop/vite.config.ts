f297cd592f0588a19899dd83f1cfac2c11307709
MiloBN1 <baidildinmiras608@gmail.com>
2024-07-24 14:17:43 +0500

    added slider animations and custom indicators

--- watches-shop/vite.config.ts
+++ watches-shop/vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +11,9 @@ export default defineConfig({
     vueJsx(),
     // vueDevTools(),
   ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
